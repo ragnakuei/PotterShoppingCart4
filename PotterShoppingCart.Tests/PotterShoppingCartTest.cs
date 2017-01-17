8b9fe2d5f6599a27a4daf5d85c763580ff561c2e
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-01-17 22:26:57 +0800

    第一個紅燈

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCartLib;
+
+namespace PotterShoppingCart.Tests
+{
+    /// <summary>
+    /// PotterShoppingCartTest 的摘要描述
+    /// </summary>
+    [TestClass]
+    public class ShoppingCartForPotterTest
+    {
+        [TestMethod]
+        public void Payment_第1集買一本_應為100()
+        {
+            // arrange
+            var target = new ShoppingCart();
+            var shoppingCartItems = new List<ShoppingCartItem> {
+                new ShoppingCartItem(1,"哈利波特第一集",1)
+            };
+
+            // act:結帳
+            decimal actual = target.CheckOut(shoppingCartItems);
+
+            // assert
+            decimal expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ShoppingCartLib/ShoppingCartItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingCartLib
+{
+    public class ShoppingCart
+    {
+        public decimal CheckOut(List<ShoppingCartItem> shoppingCartItems)
+        {
+            return 0;
+        }
+    }
+
+    public class ShoppingCartItem
+    {
+        private int _productId;
+        private string _productName;
+        private int _amount;
+
+        public ShoppingCartItem(int productId, string productName, int amount)
+        {
+            this._productId = productId;
+            this._productName = productName;
+            this._amount = amount;
+        }
+    }
+}
