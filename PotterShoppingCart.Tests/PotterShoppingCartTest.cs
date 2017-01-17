d02a27b2c05bfdf54d87e8a94f174b1e3cf333ff
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-01-17 22:39:06 +0800

    第四個紅燈

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -66,5 +66,25 @@ namespace PotterShoppingCart.Tests
             decimal expected = 270;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Payment_第1_2_3_4集各買一本_應為320()
+        {
+            // arrange
+            var target = new ShoppingCart();
+            var shoppingCartItems = new List<ShoppingCartItem> {
+                new ShoppingCartItem(1,"哈利波特第一集",1),
+                new ShoppingCartItem(2,"哈利波特第二集",1),
+                new ShoppingCartItem(3,"哈利波特第三集",1),
+                new ShoppingCartItem(4,"哈利波特第四集",1)
+            };
+
+            // act:結帳
+            decimal actual = target.CheckOut(shoppingCartItems);
+
+            // assert
+            decimal expected = 320;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
